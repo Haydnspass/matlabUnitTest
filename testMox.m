fb4be96e3767249e646b12344cef840f366887b4
Lucas-Raphael Müller <lucasraphaelmueller@gmail.com>
2017-01-19 11:16:08 +0100

    Other test

--- testMox.m
+++ testMox.m
@@ -3,3 +3,5 @@ function test_suite = testMox
     
 function test_abs_scalar
         assertTrue(abs(-1) == 1)
+        assertEqual(calcWasserwert(1,2,3,4),-8.3792)
+        assertElementsAlmostEqual(calcWasserwert(1,2,3,4),-8.3792)
